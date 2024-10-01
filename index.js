6e0667edf679687d18450293deb4a1df5611a6df
dheenadha <dheenadhaayalan2004@gmail.com>
2024-10-01 15:14:57 +0530

    error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 const { MongoClient } = require("mongodb");
+const bcrypt = require("bcryptjs");
 
 const URL ="mongodb+srv://dheena:dheena@cluster0.exzcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 app.use(cors());
@@ -38,44 +39,57 @@ app.post("/register", async (req, res) => {
     })
    }
 
-
-
-
 })
 
 app.post("/login", async (req, res) => {
-    try {
-     const connection = await MongoClient.connect(URL);
- 
-     // 2. Select the Database
-     const db = connection.db("tour");
- 
-     // 3. Select the collection
-     const collection = db.collection("login");
-     if (!req.body.email) {
-         res.status(400).json({
-           message: "Please create correct email",
-         });
-     }else{
-     // 4. Do the Operation (Insert, Read, Update, Delete)
-     await collection.insertOne(req.body);
-     // 5. Close the connection
-   await connection.close();
-   res.json({
-       message: "Created Sucessfully",
-     }); 
-     }
- 
-    } catch (error) {
-     res.status(500).json({
-         message: "Something went wrong",
-     })
+  try {
+    // 1.Connect to Database Server
+    const connection = await MongoClient.connect(URL);
+
+    // 2.Select the database
+    const db = connection.db("tour");
+
+    // 3.Select the collection
+    const collection = db.collection("register");
+    const user = await collection.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Incorrect Username/Password",
+      });
     }
- 
- 
- 
- 
- })
+
+    if (user.attempt && user.attempt == 3) {
+      return res.status(401).json({
+        message: "attempt execedded",
+      });
+    }
+
+    const passwordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordCorrect) {
+      await collection.findOneAndUpdate(
+        { email: req.body.email },
+        {
+          $inc: {
+            attempt: 1,
+          },
+        }
+      );
+      return res.status(401).json({
+        message: "Incorrect Username/Password",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
  
 app.listen(3000, () => {
     console.log("Webserver is running in port 3000");
