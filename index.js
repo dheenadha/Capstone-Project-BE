41133ac5640145e5ef02cb7ecb425e293c35d7a3
dheenadha <dheenadhaayalan2004@gmail.com>
2024-10-03 11:53:56 +0530

    chamge api

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const cors = require("cors")
 const app = express();
 const { MongoClient } = require("mongodb");
 const bcrypt = require("bcryptjs");
+const jwt= require("jsonwebtoken")
 
 const URL ="mongodb+srv://dheena:dheena@cluster0.exzcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const SECRET_KEY =
+  "CKSNBMSLZFHGXdgwufgvbhawmaeo4378rtgxyuhjzbdxfxckquebivjqexiv";
 app.use(cors());
 app.use(express.json())
 let users=[]
@@ -19,11 +22,10 @@ app.post("/register", async (req, res) => {
 
     // 3. Select the collection
     const collection = db.collection("register");
-    if (!req.body.username) {
-        res.status(400).json({
-          message: "Please create correct name",
-        });
-    }else{
+
+     const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    
     // 4. Do the Operation (Insert, Read, Update, Delete)
     await collection.insertOne(req.body);
     // 5. Close the connection
@@ -31,7 +33,7 @@ app.post("/register", async (req, res) => {
   res.json({
       message: "Created Sucessfully",
     }); 
-    }
+    
 
    } catch (error) {
     res.status(500).json({
@@ -43,6 +45,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   try {
+    console.log(req.body)
     // 1.Connect to Database Server
     const connection = await MongoClient.connect(URL);
 
@@ -51,42 +54,48 @@ app.post("/login", async (req, res) => {
 
     // 3.Select the collection
     const collection = db.collection("register");
+   
     const user = await collection.findOne({ email: req.body.email });
+    console.log(user)
+    //if (!user) {
+      //return res.status(404).json({
+       // message: "Incorrect Username/Password",
+     // });
+    //}
 
-    if (!user) {
-      return res.status(404).json({
-        message: "Incorrect Username/Password",
-      });
-    }
+   // if (user.attempt && user.attempt == 5) {
+     // return res.status(401).json({
+       // message: "attempt execedded",
+     // });
+    //}
 
-    if (user.attempt && user.attempt == 3) {
-      return res.status(401).json({
-        message: "attempt execedded",
-      });
-    }
+//    const passwordCorrect = await bcrypt.compare(
+//      req.body.password,
+ //     user.password
+  //  );
+//console.log(passwordCorrect)
+//    if (!passwordCorrect) {
+//      await collection.findOneAndUpdate(
+//        { email: req.body.email },
+//{
+ //         $inc: {
+ //           attempt: 1,
+ //         },
+ //       }
+ //     );
+ //     return res.status(401).json({
+  //      message: "Incorrect Username/Password",
+  //    });
+  //  }
+ //   await connection.close();
+    // Generate Token
+    const token = jwt.sign({ id: user._id }, SECRET_KEY);
 
-    const passwordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-
-    if (!passwordCorrect) {
-      await collection.findOneAndUpdate(
-        { email: req.body.email },
-        {
-          $inc: {
-            attempt: 1,
-          },
-        }
-      );
-      return res.status(401).json({
-        message: "Incorrect Username/Password",
-      });
-    }
+    return res.json({ message: token });
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "Something went wrong",
+      message: "Incorrect Username/Password",
     });
   }
 });
